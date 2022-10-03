6376deddbfa557c0f80cd8a58cb406c7bafc803e
jozog13 <jessica.ozog@duke.edu>
2022-10-03 13:54:56 -0400

    working on problem set 3 part 1

--- ProblemSet3_Part1.py
+++ ProblemSet3_Part1.py
@@ -1,8 +1,28 @@
-/*-PS3: Code Block 1--*/
+#-------------------------------------------------------------
+# ProblemSet3_Partl.py
+#
+# Description: Code for problem set 3
+#
+# Author: Jess Ozog (jo158@duke.edu)
+# Date:   Fall 2022
+#--------------------------------------------------------------
+#%% Task 1
+#/*-PS3: Code Block 1--*/
 
 mountain = "Denali"
 nickname = 'Mt. McKinley'
-elevation == 20322 
+elevation = 20322 
 
-print (mountain + ", formerly known as "" + nickname + ",)
-print ()"is " + elevation + '' above sea level.' )
+#print (mountain + ", formerly known as "" + nickname + ",)
+#print ()"is " + elevation + '' above sea level.' )
+
+print(f"{mountain}, formerly\nknown as \"{nickname}\" \nis {elevation} above sea level")
+
+#%% Task 2
+data_folder = "W:\859_data\\tri_state"
+data_list = ["roads.shp", "road_types.dbf", "naip_imagery.tif"]
+user_item = "streams.shp"
+data_list.append(user_item)
+
+for x in data_list:
+    print(x)
