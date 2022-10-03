c85bc322c2ed4dacb25281b3fc193a14aecd0854
jozog13 <jessica.ozog@duke.edu>
2022-10-03 12:51:22 -0400

    Create ProblemSet3_Part1.py

---
+++ ProblemSet3_Part1.py
@@ -0,0 +1,8 @@
+/*-PS3: Code Block 1--*/
+
+mountain = "Denali"
+nickname = 'Mt. McKinley'
+elevation == 20322 
+
+print (mountain + ", formerly known as "" + nickname + ",)
+print ()"is " + elevation + '' above sea level.' )
