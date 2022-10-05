9cb2c5b3a91666ff7358a75d82b3553ccdc28770
jozog13 <jessica.ozog@duke.edu>
2022-10-05 14:57:10 -0400

    Update ProblemSet3_Part1.py

--- ProblemSet3_Part1.py
+++ ProblemSet3_Part1.py
@@ -1,11 +1,12 @@
 #-------------------------------------------------------------
 # ProblemSet3_Partl.py
 #
-# Description: Code for problem set 3
+# Description: Code for Problem Set 3 - Part 1 (tasks 1-3)
 #
 # Author: Jess Ozog (jo158@duke.edu)
 # Date:   Fall 2022
 #--------------------------------------------------------------
+
 #%% Task 1
 #/*-PS3: Code Block 1--*/
 
@@ -20,9 +21,9 @@ print(f"{mountain}, formerly\nknown as \"{nickname}\" \nis {elevation} above sea
 
 #%% Task 2
 
-data_folder = "W:\859_data\\tri_state"
+data_folder = r"W:\859_data\tri_state"
 data_list = ["roads.shp", "road_types.dbf", "naip_imagery.tif"]
-data_list = list(data_list)
+#data_list = list(data_list)
 user_item = "streams.shp"
 data_list.append(user_item)
 
@@ -32,4 +33,15 @@ for item in data_list:
 #%% Task 3
 
 user_numbers=[]
-user_int = int(input("Enter an integer: "))
+
+for i in range(1,4):
+    user_int = int(input("Enter an integer: "))
+    user_numbers.append(user_int)
+    i += i
+user_numbers.sort()
+print(user_numbers[2])
+
+#%% Task 3 - Challenge 
+
+user_numbers.sort(reverse=True)
+print(user_numbers)
